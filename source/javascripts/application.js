601b54776bbe809b5ed27ef8cb26034c59340b85
Claudine Pradal <claudinecartailler@gmail.com>
2018-03-20 14:36:25 +0100

    Site improvement

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,21 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+
+function hiddenVisit() {
+  const buttons = document.querySelectorAll(".visit-button");
+  const visitData = document.querySelectorAll(".visit-details");
+  console.log(visitData);
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      let visitCategory = "panel-" + button.dataset.panel;
+      visitData.forEach((element) => {
+          element.classList.remove('visit-show');
+      });
+      document.querySelector("." + visitCategory).classList.add('visit-show');
+    });
+  });
+};
+
+hiddenVisit();
